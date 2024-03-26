458e381d7939f751aca0ab6eb700bc8494a1c839
Brandon Trump <brandontrump101@gmail.com>
2024-03-26 23:29:54 +1300

    feat: added search field & categories component

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,8 +1,21 @@
-import { View, SafeAreaView, Image, Text } from "react-native";
+import {
+  View,
+  SafeAreaView,
+  Image,
+  Text,
+  TextInput,
+  ScrollView,
+} from "react-native";
 import React, { useLayoutEffect } from "react";
 import { useNavigation } from "@react-navigation/native";
 import SafeAreaViewAndroid from "@/components/SafeAreaViewAndroid";
-import { ChevronDownIcon } from "react-native-heroicons/outline";
+import {
+  ChevronDownIcon,
+  UserIcon,
+  MagnifyingGlassIcon,
+  AdjustmentsVerticalIcon,
+} from "react-native-heroicons/outline";
+import Categories from "@/components/Categories";
 
 export default function HomeScreen() {
   const navigation = useNavigation();
@@ -17,9 +30,10 @@ export default function HomeScreen() {
       <View
         style={{
           flexDirection: "row",
-          paddingBottom: 3,
+          paddingBottom: 16,
           alignItems: "center",
           marginHorizontal: 4,
+          paddingHorizontal: 8,
           gap: 8,
         }}
       >
@@ -29,12 +43,12 @@ export default function HomeScreen() {
           style={{
             height: 28,
             width: 28,
-            backgroundColor: "gray",
+            backgroundColor: "rgb(209 213 219)",
             padding: 4,
             borderRadius: 100,
           }}
         />
-        <View>
+        <View style={{ flex: 1 }}>
           <Text
             style={{
               fontWeight: "bold",
@@ -45,12 +59,57 @@ export default function HomeScreen() {
           >
             Deliver Now
           </Text>
-          <Text style={{ fontWeight: "bold", fontSize: 20, lineHeight: 28 }}>
+          <Text
+            style={{
+              fontWeight: "bold",
+              fontSize: 20,
+              lineHeight: 28,
+            }}
+          >
             Current Location
             <ChevronDownIcon size={20} color="#00CCBB" />
           </Text>
         </View>
+
+        <UserIcon size={35} color="#00CCBB" />
       </View>
+
+      {/* Search  */}
+      <View
+        style={{
+          flexDirection: "row",
+          alignItems: "center",
+          gap: 8,
+          paddingBottom: 8,
+          marginHorizontal: 4,
+        }}
+      >
+        <View
+          style={{
+            flexDirection: "row",
+            marginLeft: 8,
+            flex: 1,
+            gap: 8,
+            backgroundColor: "#e5e7eb",
+            padding: 12,
+            alignItems: "center",
+          }}
+        >
+          <MagnifyingGlassIcon color="gray" size={20} />
+          <TextInput
+            placeholder="Restaurants and cuisines"
+            keyboardType="default"
+          />
+        </View>
+
+        <AdjustmentsVerticalIcon color="#00CCBB" />
+      </View>
+
+      <ScrollView>
+        {/* Categories  */}
+        <Categories />
+        {/* Featured rows */}
+      </ScrollView>
     </SafeAreaView>
   );
 }
