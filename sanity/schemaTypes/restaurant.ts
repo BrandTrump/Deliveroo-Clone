2a3db714bc4929fcb1ea032e6c921de431610a2f
Brandon Trump <brandontrump101@gmail.com>
2024-04-02 16:39:46 +1300

    feat: created restaurant schema

---
+++ sanity/schemaTypes/restaurant.ts
@@ -0,0 +1,42 @@
+import {defineField, defineType} from 'sanity'
+
+export default defineType({
+  name: 'restaurant',
+  title: 'Restaurant',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'name',
+      title: 'Restaurant Name',
+      type: 'string',
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: 'short_description',
+      title: 'Short Description',
+      type: 'string',
+      validation: (Rule) => Rule.max(200),
+    }),
+    defineField({
+      name: 'image',
+      title: 'Image of the Restaurant',
+      type: 'image',
+    }),
+    defineField({
+      name: 'lat',
+      title: 'Latitude of the Restaurant',
+      type: 'number',
+    }),
+    defineField({
+      name: 'long',
+      title: 'Longitude of the Restaurant',
+      type: 'number',
+    }),
+    defineField({
+      name: 'address',
+      title: 'Restaurant address',
+      type: 'string',
+      validation: (Rule) => Rule.required(),
+    }),
+  ],
+})
